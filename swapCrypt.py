f6c6a00e5f3657a9792d5d7c11b70b073591f96c
maxander420-boop <maxander420@outlook.de>
2025-11-07 18:31:04 +0100

    Adds Cryptions algorithm

--- swapCrypt.py
+++ swapCrypt.py
@@ -5,3 +5,23 @@ def swapbin(char):
         return "0"
     else:
         return None
+    
+
+
+def crypt(key,string):
+    temptf = "0"
+    cryptedtext = ""
+    for i in range(len(string)):
+        if key[i] == "1":
+            temptf = swapbin(temptf)
+        else:
+            pass
+        if temptf == "1":
+            cryptedtext += swapbin(string[i])
+        else:
+            cryptedtext += string[i]
+    return cryptedtext
+
+
+if __name__ == "__main__":
+    print(crypt(key="110",string="101"))
