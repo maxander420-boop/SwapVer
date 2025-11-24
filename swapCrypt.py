72197706f55255875728ed5e13c457958f4ee7c5
maxander420-boop <maxander420@outlook.de>
2025-11-25 00:32:52 +0100

    Added comments, fixed decraptstring and renammed it ti decryptetstring

--- swapCrypt.py
+++ swapCrypt.py
@@ -7,7 +7,7 @@ def swapbin(char):
         return None
     
 
-
+#The main funktion that is needed to crypt an bin string with an bin key
 def swapcrypt(key,string):
     temptf = "0"
     cryptedtext = ""
@@ -21,18 +21,28 @@ def swapcrypt(key,string):
         else:
             cryptedtext += string[i]
     return cryptedtext
+
+#funktion zum entschlüsseln von bin string
 def swapdecrypt(key,string):
     return swapcrypt(key=key,string=string)
+
+#funktion zum generieren von einem key nur der key wird durch den string generiert indem der bin string mit sich selbst verschlüsselt wird
 def genkey(string):
     return swapcrypt(key=swapcrypt(key=string,string=string),string=string)
+
+#funktion die einen char to einen byte string convertiert
 def chartobinstring(char):
     bi = bin(ord(char))
     return bi[2:len(bi)]
+
+#funktion die mithilfe vin chartobin einen string in einen bin string convertiert
 def stringtobin(string):
     bintext = ""
     for i in string:
         bintext += chartobinstring(i)
     return bintext
+
+#funktion die einen string verschlüsselt mithilfe eines strings die ausgabe ist ein verschlüsselter string
 def cryptstring(key,string):
     print(f"string1: {string},Key1:{key}")
     string = swapcrypt(stringtobin(key),stringtobin(string))
@@ -44,11 +54,15 @@ def cryptstring(key,string):
     print(f"string1: {string},Key1:{key}")
     print(f"text:{text}")
     return text
-def decracptstring(key,string):
+
+#funktion die einen verschlüsselten string entschlüsselt
+def decrycptstring(key,string):
     print(f"Key:{key} String:{string}")
-    return swapcrypt(stringtobin(key),stringtobin(string))
+    return cryptstring(key,string)
+
+#testet jede wichtige funktion
 if __name__ == "__main__":
     print(swapcrypt(key="110",string="101"))
     print(genkey("010010101010001"))
     print(cryptstring("f","h"))
-    print(decracptstring("e","e"))
+    print(decrycptstring("e","e"))
